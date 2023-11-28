a611ff813f1e309a1bb5da57bea526cb4ce5331e
Sofía Huppertz <sofia.huppertz@gmail.com>
2023-11-28 23:09:32 +0100

    exec returns

--- srcs/helpers/builtins/exit.c
+++ srcs/helpers/builtins/exit.c
@@ -3,20 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: shuppert <shuppert@student.42.fr>          +#+  +:+       +#+        */
+/*   By: sofia <sofia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 18:47:47 by shuppert          #+#    #+#             */
-/*   Updated: 2023/11/27 18:57:46 by shuppert         ###   ########.fr       */
+/*   Updated: 2023/11/28 23:08:54 by sofia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../headers/minishell.h"
 
+
+static int	print_numeric_arg_error(const char *arg)
+{
+	ft_putstr_fd("minishell: exit: ", 2);
+	ft_putstr_fd((char *)arg, 2);
+	ft_putendl_fd(": numeric argument required", 2);
+	return (255);
+}
+
 int	exit_cmd(const char **cmd)
 {
+	int arg_len;
+	int all_digit;
+
+	if (cmd[1])
+		arg_len = ft_strlen(cmd[1]);
 	g_sig.exit_shell = 1;
 	ft_putendl_fd("exit 👋", 2);
-	if (cmd[1] && ft_is_all(cmd[1], &ft_isdigit) && cmd[2])
+	all_digit = ft_is_all(cmd[1], &ft_isdigit);
+	if (cmd[1] && all_digit && cmd[2] && arg_len <= 20)
 	{
 		ft_putendl_fd("minishell: exit: too many arguments", 2);
 		g_sig.exit_shell = 0;
@@ -24,13 +39,8 @@ int	exit_cmd(const char **cmd)
 	}
 	else if (cmd[1])
 	{
-		if (!(ft_is_all(cmd[1], &ft_isdigit)))
-		{
-			ft_putstr_fd("minishell: exit: ", 2);
-			ft_putstr_fd((char *)cmd[1], 2);
-			ft_putendl_fd(": numeric argument required", 2);
-			return (255);
-		}
+		if (!all_digit || arg_len > 20)
+			return (print_numeric_arg_error((const char *)cmd[1]));
 		else
 			return (ft_atoi(cmd[1]));
 	}
