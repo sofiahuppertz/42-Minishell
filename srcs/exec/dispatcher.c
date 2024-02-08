3b5f03bcab7c3c95130689a472d938fde3a59690
Sofía Huppertz <sofia.huppertz@gmail.com>
2024-02-08 16:46:02 +0100

    .

--- srcs/exec/dispatcher.c
+++ srcs/exec/dispatcher.c
@@ -6,7 +6,7 @@
 /*   By: sofia <sofia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 18:46:41 by shuppert          #+#    #+#             */
-/*   Updated: 2024/02/07 19:14:23 by sofia            ###   ########.fr       */
+/*   Updated: 2024/02/08 16:45:44 by sofia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,9 +42,9 @@ int	dispatcher(t_cmd_line **cmd_line, pid_t *pid, int num_cmds)
 		if (num_cmds == 1 && is_builtin(simple_cmd->argv[0]))
 			*status_pointer() = exec_builtin((const char **)(*cmd_line)->argv,
 					(*cmd_line)->fd_out, 0);
-		else if (!simple_cmd->argv[0] && (simple_cmd->fd_out != 1
-				|| simple_cmd->fd_in != 0))
-			*status_pointer() = 0;
+		//else if (!simple_cmd->argv[0] && (simple_cmd->fd_out != 1
+		//		|| simple_cmd->fd_in != 0))
+		//	*status_pointer() = 0;
 		else
 			handle_multiple_commands(num_cmds, pid, &cmd_line, &simple_cmd);
 	}
