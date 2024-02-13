9b9eced65c53a9bfde7baa3a1993344a7f34f221
Sofía Huppertz <sofia.huppertz@gmail.com>
2024-02-13 22:22:34 +0100

    .

--- srcs/helpers/builtins/unset.c
+++ srcs/helpers/builtins/unset.c
@@ -3,33 +3,41 @@
 /*                                                        :::      ::::::::   */
 /*   unset.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: shuppert <shuppert@student.42.fr>          +#+  +:+       +#+        */
+/*   By: sofia <sofia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 18:48:11 by shuppert          #+#    #+#             */
-/*   Updated: 2024/02/13 19:02:03 by shuppert         ###   ########.fr       */
+/*   Updated: 2024/02/13 22:21:58 by sofia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../headers/minishell.h"
 
+static int	is_value_in_env(const char *value)
+{
+	t_env	*env;
+	int		len;
+
+	env = *get_adress_envp();
+	len = ft_strlen(value);
+	while (env)
+	{
+		if (ft_strncmp(env->str, value, len) == 0 
+			&& (env->str[len] == '\0' || env->str[len] == '='))
+			return (1);
+		env = env->next;
+	}
+	return (0);
+}
+
 int	unset(const char **args)
 {
 	int		i;
-	int		stop;
-	char	*temp;
 
 	i = 1;
-	stop = 0;
-	temp = NULL;
 	while (args[i])
 	{
-		temp = e_get_val(args[i], *get_adress_envp());
-		if (!envp_is_valid_varname(args[i]))
-			stop = 1;
-		else if (temp == NULL || ft_strcmp(temp, "") == 0)
-			stop = 1;
-		ft_memdel((void *)temp);
-		if (!stop)
+		int value_in_env = is_value_in_env(args[i]);
+		if (value_in_env && ft_is_all(args[i], ft_isalnum))
 			remove_var(args[i], get_adress_envp());
 		i++;
 	}
