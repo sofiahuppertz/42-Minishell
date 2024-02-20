d1e92ae2c376373bc0df19f9fe0056f79754cf29
Sofía Huppertz <sofia.huppertz@gmail.com>
2024-02-20 08:37:38 +0100

    .

--- srcs/helpers/heredoc/read_into_heredoc.c
+++ srcs/helpers/heredoc/read_into_heredoc.c
@@ -3,18 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   read_into_heredoc.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: shuppert <shuppert@student.42.fr>          +#+  +:+       +#+        */
+/*   By: sofia <sofia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 18:52:23 by shuppert          #+#    #+#             */
-/*   Updated: 2024/02/19 19:29:02 by shuppert         ###   ########.fr       */
+/*   Updated: 2024/02/20 08:34:43 by sofia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../headers/minishell.h"
 
+static void process_line(char **line, t_env *envp, int fd)
+{
+	if ((*line)[0] != '\0')
+		expand_cmd(line, envp);
+	ft_putendl_fd(*line, fd);
+	ft_memdel((void *)*line);
+}
+
 int	read_into_heredoc(int fd, char *limitor)
 {
-	char	*here_line;
+	char	*line;
 	int		new_fd;
 	t_env	*envp;
 
@@ -24,8 +32,8 @@ int	read_into_heredoc(int fd, char *limitor)
 	*cmd_in_progress() = 0;
 	while (1)
 	{
-		here_line = readline("heredoc> ");
-		if (here_line == NULL && g_caught_signal != SIGINT)
+		line = readline("heredoc> ");
+		if (line == NULL && g_caught_signal != SIGINT)
 			return (0);
 		if (g_caught_signal == SIGINT)
 		{
@@ -33,16 +41,13 @@ int	read_into_heredoc(int fd, char *limitor)
 			dup2(new_fd, STDIN_FILENO);
 			break ;
 		}
-		if (ft_strcmp(limitor, here_line) == 0)
+		if (ft_strcmp(limitor, line) == 0)
 			break ;
-		if (here_line[0] != '\0')
-			expand_cmd(&here_line, envp);
-		ft_putendl_fd(here_line, fd);
-		ft_memdel((void *)here_line);
+		process_line(&line, envp, fd);
 	}
 	*cmd_in_progress() = 1;
 	*get_heredoc_flag() = 0;
-	ft_memdel((void *)here_line);
+	ft_memdel((void *)line);
 	close(new_fd);
 	return (0);
 }
--- srcs/helpers/spaces/strcpy_adding_spaces.c
+++ srcs/helpers/spaces/strcpy_adding_spaces.c
@@ -3,42 +3,45 @@
 /*                                                        :::      ::::::::   */
 /*   strcpy_adding_spaces.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: shuppert <shuppert@student.42.fr>          +#+  +:+       +#+        */
+/*   By: sofia <sofia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 18:55:01 by shuppert          #+#    #+#             */
-/*   Updated: 2024/02/19 19:29:36 by shuppert         ###   ########.fr       */
+/*   Updated: 2024/02/20 08:37:01 by sofia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../headers/minishell.h"
 
-void	strcpy_adding_spaces(char **new, char *str)
+void process_char(char **new_str, char *str, int *idx)
 {
-	char	*new_str;
-	int		idx;
+	if (str[*idx] == '$' && str[*idx + 1] == '\'')
+		(*idx)++;
+	else if (find_open_quotes(str, *idx) != 2 && str[*idx] == '$' && str[*idx + 1] != '\"')
+		*(*new_str)++ = (char)(-str[(*idx)++]);
+	else if (!find_open_quotes(str, *idx) && is_delim(str, *idx))
+	{
+		*(*new_str)++ = ' ';
+		*(*new_str)++ = str[(*idx)++];
+		if (find_open_quotes(str, *idx) == 0 && (str[*idx] == '>' || str[*idx] == '<'))
+			*(*new_str)++ = str[(*idx)++];
+		*(*new_str)++ = ' ';
+	}
+	else
+		*(*new_str)++ = str[(*idx)++];
+}
+
+void strcpy_adding_spaces(char **new, char *str)
+{
+	char *new_str;
+	int idx;
 
 	if (!new || !(*new) || !str)
-		return ;
+		return;
 	idx = 0;
 	new_str = *new;
 	while (str[idx])
 	{
-		if (str[idx] == '$' && str[idx + 1] == '\'')
-			idx += 1;
-		if (find_open_quotes(str, idx) != 2 && str[idx] == '$' && str[idx
-			+ 1] != '\"')
-			*new_str++ = (char)(-str[idx++]);
-		else if (!find_open_quotes(str, idx) && is_delim(str, idx))
-		{
-			*new_str++ = ' ';
-			*new_str++ = str[idx++];
-			if (find_open_quotes(str, idx) == 0 && (str[idx] == '>'
-					|| str[idx] == '<'))
-				*new_str++ = str[idx++];
-			*new_str++ = ' ';
-		}
-		else
-			*new_str++ = str[idx++];
+		process_char(&new_str, str, &idx);
 	}
 	*new_str = '\0';
 }
